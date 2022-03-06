94f52aeda0b3e20eae0bba0d3ecd27517486e805
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-03-06 20:36:37 +0900

    first commit

---
+++ DUALSHOCK2.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file DUALSHOCK2.cpp
+ * @brief
+ * @date Mar 6, 2022
+ */
+#include <DUALSHOCK2.hpp>
+
+DUALSHOCK2::DUALSHOCK2() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ DUALSHOCK2.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file DUALSHOCK2.hpp
+ * @brief
+ * @date Mar 6, 2022
+ */
+#ifndef DUALSHOCK2_HPP_
+#define DUALSHOCK2_HPP_
+
+class DUALSHOCK2 {
+public:
+	DUALSHOCK2();
+};
+
+#endif /* DUALSHOCK2_HPP_ */
