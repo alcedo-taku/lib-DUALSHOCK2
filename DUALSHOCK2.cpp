4d52b271890601ae2b53f2dd77dc8cb762dd31c7
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-03-09 00:35:00 +0900

    feat: スティックの中心からの値を取得できるようにした

--- DUALSHOCK2.cpp
+++ DUALSHOCK2.cpp
@@ -44,16 +44,37 @@ void DUALSHOCK2::init(uint32_t timeout){
 	send_command(CMD::PRESSURE_ENABLE, timeout);
 	send_command(CMD::PRESSURE_ENABLE, timeout);
 	send_command(CMD::PRES_TRANS_START, timeout);
+}
 
+void DUALSHOCK2::reset_stick(uint32_t timeout){
 	send_command(CMD::READ_DATA_EX, timeout);
+	std::copy(receive_data.begin()+5, receive_data.begin()+9, stick_offset.begin());
 }
 
 /**
  * コントローラのデータを更新する関数
  * @param timeout SPI通信のtimeout
+ * @param stk_0_range スティックの0の範囲を定義
  */
-void DUALSHOCK2::update(uint32_t timeout){
+void DUALSHOCK2::update(uint32_t timeout, uint8_t stk_0_range){
+	// receive_data を更新
 	send_command(CMD::READ_DATA_EXEX, timeout);
+
+	// スティックの値を更新
+	stick_data[0] =   (receive_data[5] - stick_offset[0] );
+	stick_data[1] = - (receive_data[6] - stick_offset[1] );
+	stick_data[2] =   (receive_data[7] - stick_offset[2] );
+	stick_data[3] = - (receive_data[8] - stick_offset[3] );
+	// 0の範囲内ならスティックの値を0にする
+	for(uint8_t i=0; i < 4; i++){
+		if ( stk_0_range < stick_data[i] ){
+			stick_data[i] -= stk_0_range;
+		}else if ( stick_data[i] < -stk_0_range ){
+			stick_data[i] += stk_0_range;
+		}else {
+			stick_data[i] = 0;
+		}
+	}
 }
 
 
@@ -73,4 +94,12 @@ std::array<uint8_t, 9> DUALSHOCK2::get_data_ex(){
  */
 std::array<uint8_t,21> DUALSHOCK2::get_data_exex(){
 	return receive_data;
+}
+
+/**
+ * コントローラのスティックの値を取得する関数
+ * @return コントローラのスティックの値
+ */
+std::array<int16_t, 4> DUALSHOCK2::get_stick_data(){
+	return stick_data;
 }
